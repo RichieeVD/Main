ac247b13d83606a786e14cda12fc28e7694d71a6
rvd <nseries_nokia@list.ru>
2025-05-16 03:56:00 +0400

    Task 2

--- Main.java
+++ Main.java
@@ -8,6 +8,9 @@ public class Main {
         System.out.println("cat: " + cat);
         System.out.println("paper: " + paper);
 
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
     }
 
 }
